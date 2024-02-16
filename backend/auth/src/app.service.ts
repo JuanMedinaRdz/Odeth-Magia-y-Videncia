0e745d74ddda79dbc28496f2478463987ca762c2
Bladimir <oirbl@hotmail.com>
2024-02-15 20:07:29 -0600

    🦺 add login endpoint

--- backend/auth/src/app.service.ts
+++ backend/auth/src/app.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { UserDTO } from "./dto/user.dto";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly admin = {
+    username: "maria",
+    password: process.env.PASSWORD,
+  };
+
+  validateUser(user: UserDTO): boolean {
+    return user.name === this.admin.username && user.password === this.admin.password;
   }
 }
